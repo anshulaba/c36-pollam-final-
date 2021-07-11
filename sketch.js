bfe308cf0bc2fbaf540c02a5beb43ef00cc19457
anshulaba <72729339+anshulaba@users.noreply.github.com>
2021-07-11 10:58:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var canvas,database,gameState=0,playerCount;
+var form,game,player;
+
+function setup()
+{
+  canvas=createCanvas(400,400);
+  database=firebase.database();
+game=new Game();
+game.getState();
+game.start();
+ 
+
+}
+
+function draw()
+{
+
+}
